3787a79626b5c0ab92c0310561100774a996e187
uuo00_n <uuo00_n@outlook.com>
2022-11-24 16:25:40 +0800

    重定向与转发 :)
    Signed-off-by: Administrator <admin@example.com>

---
+++ src/com/jqe/servlet/ServletDemo.java
@@ -0,0 +1,31 @@
+package com.jqe.servlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@javax.servlet.annotation.WebServlet(name = "ServletDemo")
+public class ServletDemo extends javax.servlet.http.HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+        String content = request.getParameter("content");
+        PrintWriter out = response.getWriter();
+        if (content.equals("JSP")) {
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("show.jsp");
+            requestDispatcher.forward(request, response);
+        } else {
+            response.sendRedirect("show.jsp");
+        }
+    }
+}
---
+++ web/show.jsp
@@ -0,0 +1,25 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2022/11/24
+  Time: 11:02
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" import="java.util.*" %>
+<html>
+<head>
+    <title>show</title>
+</head>
+<body>
+<b>如果index.jsp页面提交的数据为JSP，那么Servlet就会转发得到此页面，所以可以通过request对象获取提交的数据 ，如果数据不是JSP，那么将获取不到提交的信息。</b>
+<br>
+<%
+    String content = request.getParameter("content");
+    if (content != null) {
+%>
+提交的数据是：<%=content%>
+<%} else {%>
+没有获取到提交的数据
+<%}%>
+</body>
+</html>
