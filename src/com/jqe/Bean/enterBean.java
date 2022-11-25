96dc1d8c4a114f23836cac2af8f8aa742a749d01
Administrator <admin@example.com>
2022-11-25 08:34:15 +0800

    重定向与转发(Bean) :)
    Signed-off-by: Administrator <admin@example.com>

---
+++ src/com/jqe/Bean/enterBean.java
@@ -0,0 +1,17 @@
+package com.jqe.Bean;
+
+/**
+ * @author uuo00_n(Hwang Jumbo)
+ * @Date 2022/11/24 16:26
+ */
+public class enterBean {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
